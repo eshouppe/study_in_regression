20db9bdee778823fa26fd8451c0b41d23ced4915
Emily Shouppe <eshouppe@gmail.com>
2017-03-04 18:18:16 -0500

    More updates to reamde. Stub possible questions

--- services/main.py
+++ services/main.py
@@ -10,3 +10,33 @@ post_request_template = {
             "per_page": 100
         }
     }
+
+
+def num_citations_by_patent():
+    """Count how many citations a patent has.
+    Possible regression methods are poisson, OLS,
+    ridge, and lasso."""
+    pass
+
+def num_citations_of_patent():
+    """Count how many patents will cite this patent.
+    Possible regression methods are poisson, OLS,
+    ridge, and lasso."""
+    pass
+
+def patent_approval_probability():
+    """Probability that a patent will be approved.
+    Possible regression method is logistic."""
+    pass
+
+def num_days_to_approve():
+    """Number of days between submission and approval.
+    Possible regression methods are poisson, OLS,
+    ridge, and lasso."""
+    pass
+
+def num_patents_approved_by_month():
+    """Monthly trend in volume of patents approved.
+    Possible regression methods are poisson, OLS,
+    ridge, and lasso."""
+    pass
---
+++ services/model.py
@@ -0,0 +1,4 @@
+import numpy as np
+import statsmodels.api as sm
+from sklearn import linear_model
+
